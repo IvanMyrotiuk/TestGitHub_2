17fed686279dc63f0a2f8e3edab3ab5ec45fcd55
IvanMyrotiuk <ivan.myrotiuk@gmail.com>
2015-06-11 10:18:30 +0300

    first commit

---
+++ src/com/java/git/HelloGit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.java.git;
+
+/**
+ *
+ * @author John
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Hub");
+    }
+    
+}
